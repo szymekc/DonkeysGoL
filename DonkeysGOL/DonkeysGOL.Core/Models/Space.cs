35da3f22736b843233ac4247993b36717acdc24d
qronin <michal.maron@pm.me>
2018-05-10 18:49:54 +0200

    Create Core Project; Create WPF Project; Create Console Project; Fill projects with NotImplementedEx

---
+++ DonkeysGOL/DonkeysGOL.Core/Models/Space.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonkeysGOL.Core.Models
+{
+    class Space
+    {
+        public bool[][] Array { get; set; }
+    }
+}
