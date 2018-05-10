2309ee5254f36c09b3c00fc796b668b5ac5b6469
qronin <michal.maron@pm.me>
2018-05-10 19:51:41 +0200

    Create CrudeRenderer for debug purposes; create simple spacearray for test in console.main;

---
+++ DonkeysGOL/DonkeysGOL.Console/View/CrudeRenderer.cs
@@ -0,0 +1,32 @@
+﻿using DonkeysGOL.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonkeysGOL.Console.View
+{
+   public static class CrudeRenderer
+    {
+
+        /// <summary>
+        /// ONLY FOR DEBUGGING PURPOSES, IT WILL BE CHANGED, DONT RELY ON IT!
+        /// </summary>
+        public static void RenderSpace ( Space space )
+        {
+            for (int i = 0; i < space.SpaceArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < space.SpaceArray.GetLength(1) ; j++)
+                {
+                    System.Console.Write(space.SpaceArray[i,j]);
+                }
+                System.Console.WriteLine("");
+            }
+
+
+
+            
+        }
+    }
+}
--- DonkeysGOL/DonkeysGOL.Core/Models/Game.cs
+++ DonkeysGOL/DonkeysGOL.Core/Models/Game.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DonkeysGOL.Core.Models
 {
-    class Game
+    public class Game
     {
          void ApplyRules()
         {
