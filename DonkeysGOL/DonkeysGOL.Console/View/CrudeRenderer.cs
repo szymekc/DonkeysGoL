a06632feb4c750cb9efee944dbe5f0f483a5ec31
szymekc <szymekc@interia.eu>
2018-05-19 16:44:26 +0200

    Separated RenderSpace from Timer

--- DonkeysGOL/DonkeysGOL.Console/View/CrudeRenderer.cs
+++ DonkeysGOL/DonkeysGOL.Console/View/CrudeRenderer.cs
@@ -8,7 +8,7 @@ using System.Timers;
 
 namespace DonkeysGOL.Console.View
 {
-   public static class CrudeRenderer
+    public static class CrudeRenderer
     {
 
         /// <summary>
@@ -16,28 +16,28 @@ namespace DonkeysGOL.Console.View
         /// </summary>
         /// 
 
-        public static void RenderSpace ( Space space )
+        public static void PeriodicRender(Space space)
         {
-            Timer timer = new System.Timers.Timer(10000); //change period
-            timer.Elapsed += TickBoard;
+            Timer timer = new System.Timers.Timer(3000); //change period
+            timer.Elapsed += (sender, e) => RenderSpace(space, sender, e);
             timer.Enabled = true;
-            void TickBoard(object source, ElapsedEventArgs e)
+        }
+        public static void RenderState(Space space)
+        {
+            System.Console.Clear();
+            for (int i = 0; i < space.SpaceArray.GetLength(0); i++)
             {
-                System.Console.Clear();
-                for (int i = 0; i < space.SpaceArray.GetLength(0); i++)
+                for (int j = 0; j < space.SpaceArray.GetLength(1); j++)
                 {
-                    for (int j = 0; j < space.SpaceArray.GetLength(1); j++)
-                    {
-                        System.Console.Write(space.SpaceArray[i, j] ? "■" : " ");
-                        System.Console.Write(" ");
-                    }
-                    System.Console.WriteLine("");
+                    System.Console.Write(space.SpaceArray[i, j] ? "■" : " ");
+                    System.Console.Write(" ");
                 }
+                System.Console.WriteLine("");
             }
-
-
-
-            
+        }
+        public static void RenderSpace(Space space,object sender, ElapsedEventArgs e)
+        {
+            RenderState(space);        
         }
     }
 }
